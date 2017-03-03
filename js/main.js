ed409d68202c32145b229ebfd593b3072ffd2644
RuthMery <ruth.cardenas.perez@gmail.com>
2017-03-03 16:45:18 -0500

    Implementando validaciones

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+  var filters = [/^[A-Z][a-z\s]*$/g,//Para name-->Sólo letras, mayúsculas o minúsculas, acepta espacios en blanco
+                 /^[A-Z][a-z\s]*$/g,//Para lastName-->Sólo letras, mayúsculas o minúsculas, acepta espacios en blanco
+                 /^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/,
+                 /^[^password|PASSWORD][^123456|098754]\w{6,}$/g,
+                 /^[^0]*$/
+                ];
+  var data = {
+                name : document.getElementById('name').value,
+                lastName : document.getElementById('lastname').value,
+                input_email : document.getElementById('input-email').value,
+                input_password : document.getElementById('input-password').value,
+                bicycle : document.getElementById('bikes').value
+              };
+//VALIDACIONES
+  alert(filters[0].test(data.name) + " " +
+        filters[1].test(data.lastName) + " " +
+        filters[2].test(data.input_email) + " " +
+        filters[3].test(data.input_password) + " " +
+        filters[4].test(data.bicycle)
+      );
 }
